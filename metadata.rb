3e6951defbe871e068906e56ad839274de54c32d
Irving Popovetsky <irving@getchef.com>
2015-04-09 14:32:42 -0700

    Bump version and fix metadata

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 name             'supermarket-omnibus-cookbook'
-maintainer       'The Authors'
-maintainer_email 'you@example.com'
+maintainer       'Irving Popovetsky'
+maintainer_email 'irving@chef.io'
 license          'all_rights'
 description      'Installs/Configures supermarket-omnibus-cookbook'
 long_description 'Installs/Configures supermarket-omnibus-cookbook'
-version          '0.1.0'
+version          '0.2.0'
 
 depends 'chef-server-ingredient'

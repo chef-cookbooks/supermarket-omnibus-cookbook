1c2eaafc4fcc88f3546a03186a1f89f98b7e7ae6
Irving Popovetsky <irving@getchef.com>
2015-05-08 16:50:06 -0700

    Update metadata with source and issues url

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,9 @@ maintainer_email 'irving@chef.io'
 license          'all_rights'
 description      'Installs/Configures supermarket-omnibus-cookbook'
 long_description 'Installs/Configures supermarket-omnibus-cookbook'
-version          '0.2.0'
+source_url       'https://github.com/irvingpop/supermarket-omnibus-cookbook'
+issues_url       'https://github.com/irvingpop/supermarket-omnibus-cookbook/issues'
+version          '0.2.1'
+
 
 depends 'chef-server-ingredient'

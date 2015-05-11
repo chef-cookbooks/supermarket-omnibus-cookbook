2c6d58ad82e7fb39b75d5aba7ef8498b1d3b1962
Irving Popovetsky <irving@getchef.com>
2015-05-11 16:43:02 -0700

    Update that makes the package source or packagecloud repo configurable

--- Berksfile
+++ Berksfile
@@ -2,3 +2,4 @@ source "https://supermarket.getchef.com"
 
 metadata
 
+cookbook 'chef-server-ingredient', git: 'https://github.com/irvingpop/chef-server-ingredient.git'
--- metadata.rb
+++ metadata.rb
@@ -2,11 +2,10 @@ name             'supermarket-omnibus-cookbook'
 maintainer       'Irving Popovetsky'
 maintainer_email 'irving@chef.io'
 license          'all_rights'
-description      'Installs/Configures supermarket-omnibus-cookbook'
-long_description 'Installs/Configures supermarket-omnibus-cookbook'
+description      'Installs and Configures Supermarket from the Omnibus packages on packagecloud.io'
 source_url       'https://github.com/irvingpop/supermarket-omnibus-cookbook'
 issues_url       'https://github.com/irvingpop/supermarket-omnibus-cookbook/issues'
-version          '0.2.1'
+version          '0.3.0'
 
 
 depends 'chef-server-ingredient'

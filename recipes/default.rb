be4ebaaae239f8ce161430037c4cd5cf351d5378
Stephen Lauck <stephen.lauck@gmail.com>
2015-05-18 19:31:19 -0400

    add template for /etc/supermarket/supermarket.json

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,35 +4,17 @@
 #
 # Copyright (c) 2014 The Authors, All Rights Reserved.
 
-# Configure Supermarket server hostname in /etc/hosts if it isn't there (AWS)
-hostsfile_entry node['ipaddress'] do
-  hostname node.hostname
-  not_if "grep #{node.hostname} /etc/hosts"
-end
+# # Configure Supermarket server hostname in /etc/hosts if it isn't there (AWS)
+# hostsfile_entry node['ipaddress'] do
+#   hostname node.hostname
+#   not_if "grep #{node.hostname} /etc/hosts"
+# end
 
 directory '/etc/supermarket' do
   owner 'root'
   group 'root'
   mode '0755'
   action :create
-end
-
-# Sanity check the oc-id attributes
-%w(chef_server_url chef_oauth2_app_id chef_oauth2_secret).each do |attr|
-  unless node['supermarket_omnibus'][attr].is_a?(String)
-    Chef::Log.fatal("You did not set the node['supermarket_omnibus']['#{attr}'] value!")
-    Chef::Log.fatal('Please set this attribute before continuing')
-    raise
-  end
-end
-
-file '/etc/supermarket/supermarket.json' do
-  action :create
-  owner "root"
-  group "root"
-  mode "0644"
-  content JSON.pretty_generate(node['supermarket_omnibus'])
-  notifies :reconfigure, 'chef_server_ingredient[supermarket]'
 end
 
 chef_server_ingredient 'supermarket' do
@@ -47,5 +29,20 @@ chef_server_ingredient 'supermarket' do
     repository node['supermarket_package']['packagecloud_repo']
   end
 
+  notifies :reconfigure, 'chef_server_ingredient[supermarket]'
+end
+
+template '/etc/supermarket/supermarket.json' do
+  source 'supermarket.json.erb'
+  owner 'root'
+  group 'root'
+  mode "0644"
+  variables(
+        :chef_server_url => node['supermarket_omnibus']['chef_server_url'],
+        :chef_oauth2_app_id => get_supermarket_attribute('uid'),
+        :chef_oauth2_secret => get_supermarket_attribute('secret'),
+        :chef_oauth2_verify_ssl => node['supermarket_omnibus']['chef_oauth2_verify_ssl']
+  )
+  action :create
   notifies :reconfigure, 'chef_server_ingredient[supermarket]'
 end

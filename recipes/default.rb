3e8f077aa5a090b91bfc36b04507483b828a6fbf
Stephen Lauck <stephen.lauck@gmail.com>
2015-05-18 19:34:46 -0400

    remove variables for template

--- recipes/default.rb
+++ recipes/default.rb
@@ -37,12 +37,6 @@ template '/etc/supermarket/supermarket.json' do
   owner 'root'
   group 'root'
   mode "0644"
-  variables(
-        :chef_server_url => node['supermarket_omnibus']['chef_server_url'],
-        :chef_oauth2_app_id => get_supermarket_attribute('uid'),
-        :chef_oauth2_secret => get_supermarket_attribute('secret'),
-        :chef_oauth2_verify_ssl => node['supermarket_omnibus']['chef_oauth2_verify_ssl']
-  )
   action :create
   notifies :reconfigure, 'chef_server_ingredient[supermarket]'
 end

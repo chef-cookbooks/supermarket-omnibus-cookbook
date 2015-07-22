dad15103b1de96026ddc9f8a6b6cc3480f554383
Bakh Inamov <b@chef.io>
2015-07-21 19:59:47 -0700

    stable or current
    you can pick ; can't call it packagecloud

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,11 +1,11 @@
 
-default['supermarket_omnibus']['chef_server_url'] = nil
-default['supermarket_omnibus']['chef_oauth2_app_id'] = nil
-default['supermarket_omnibus']['chef_oauth2_secret'] = nil
+default['supermarket_omnibus']['chef_server_url'] = 'nil'
+default['supermarket_omnibus']['chef_oauth2_app_id'] = 'nil'
+default['supermarket_omnibus']['chef_oauth2_secret'] = 'nil'
 default['supermarket_omnibus']['chef_oauth2_verify_ssl'] = false
+
 default['supermarket_omnibus']['package_url'] = nil
 default['supermarket_omnibus']['package_version'] = nil
-
 default['supermarket_omnibus']['package_repo'] = 'chef-stable'
 # use the following to consume nightly builds of packagecloud:
 #default['supermarket_omnibus']['package_repo'] = 'chef-current'

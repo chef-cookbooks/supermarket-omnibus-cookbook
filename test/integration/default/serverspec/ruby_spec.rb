979aaa357a2c24dd690974d0cc77385ffd8f0b3a
Irving Popovetsky <irving@getchef.com>
2015-09-14 15:23:40 -0700

    Remove the Ruby serverspec, it doesn't make sense to verify the Ruby version when it can change between omnibuses

--- test/integration/default/serverspec/ruby_spec.rb
+++
@@ -1,8 +0,0 @@
-require_relative 'spec_helper'
-
-describe 'ruby' do
-  it '2.1.3 installed and used by default' do
-    cmd = command '/opt/supermarket/embedded/bin/ruby -v'
-    expect(cmd.stdout).to match 'ruby 2.1.3'
-  end
-end

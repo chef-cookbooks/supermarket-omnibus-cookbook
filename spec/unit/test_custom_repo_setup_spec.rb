bb692bb345b0a384d96006f794155141bf14a693
Yvonne Lam <yvonne@chef.io>
2016-04-27 16:23:43 -0700

    Fix multiple compounding errors in test and fixture:   * misnamed test file caused test not to run,   * misnamed node attribute in fixture,   * missing dependency in fixture cookbook.

--- spec/unit/test_custom_repo_setup_spec.rb
+++ spec/unit/test_custom_repo_setup_spec.rb
@@ -4,17 +4,13 @@
 #
 # Copyright (c) 2016 Yvonne Lam, All Rights Reserved.
 
-require 'spec_helper'
-require 'mixlib/install'
-
 describe 'test::custom_repo_setup' do
   context 'When a custom recipe is specified, the custom recipe should be used' do
     let(:chef_run) do
       runner = ChefSpec::SoloRunner.new(platform: 'redhat', version: '6.5', step_into: %w(chef_ingredient supermarket_server)) do |node|
-        node.set['supermarket_omnibus']['custom_repo_recipe'] = 'custom_repo::custom_repo_recipe'
-        node.set['supermarket_omnibus']['chef_server_url']    = 'https://chefserver.mycorp.com'
+        node.set['supermarket_omnibus']['chef_server_url'] = 'https://chefserver.mycorp.com'
         node.set['supermarket_omnibus']['chef_oauth2_app_id'] = 'blahblah'
-        node.set['supermarket_omnibus']['chef_oauth2_secret'] = 'bob_loblaw'
+        node.set['supermarket_omnibus']['chef_oauth2_secret'] = 'bob_lawblaw'
       end
       runner.converge(described_recipe)
     end
@@ -23,8 +19,8 @@ describe 'test::custom_repo_setup' do
       stub_command('grep Fauxhai /etc/hosts').and_return('33.33.33.11 Fauxhai')
     end
 
-    it 'includes the custom recipe' do
-      expect(chef_run).to include_recipe('custom_repo::custom_repo_recipe')
+    it 'includes the custom_repo_setup_recipe' do
+      expect(chef_run).to include_recipe 'custom_repo::custom_repo_recipe'
     end
 
     it 'converges successfully' do
--- test/fixtures/cookbooks/test/metadata.rb
+++ test/fixtures/cookbooks/test/metadata.rb
@@ -2,3 +2,4 @@ name 'test'
 version '0.0.1'
 
 depends 'supermarket-omnibus-cookbook'
+depends 'custom_repo'

1928d09e7e66ca69d5d864527636f7b44196f9a3
Tim Smith <tsmith@chef.io>
2017-04-11 09:34:58 -0700

    Don’t fail on older platforms that don’t know about chef_version
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license          'Apache-2.0'
 description      'Installs and Configures Supermarket from the Omnibus packages on packages.chef.io'
 source_url       'https://github.com/chef-cookbooks/supermarket-omnibus-cookbook'
 issues_url       'https://github.com/chef-cookbooks/supermarket-omnibus-cookbook/issues'
-chef_version     '>= 12.5'
+chef_version     '>= 12.5' if respond_to?(:chef_version)
 version          '2.0.1'
 
 supports 'ubuntu'

e77effeca4f00748c4d882c1425643c6b0f6a483
Tim Smith <tsmith84@gmail.com>
2019-12-19 10:16:37 -0800

    Remove if respond_to?(:chef_version) from metadata
    There's no need for this anymore
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license          'Apache-2.0'
 description      'Installs and Configures Supermarket from the Omnibus packages on packages.chef.io'
 source_url       'https://github.com/chef-cookbooks/supermarket-omnibus-cookbook'
 issues_url       'https://github.com/chef-cookbooks/supermarket-omnibus-cookbook/issues'
-chef_version     '>= 12.5' if respond_to?(:chef_version)
+chef_version     '>= 12.5'
 version          '3.0.0'
 
 supports 'ubuntu'
